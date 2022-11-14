bb02c2318f03b97cbdab9ff09ec521b2290bc552
colin <colin_bao@163.com>
2022-11-15 03:50:26 +0800

    update

--- analyzer/img_analyzer.py
+++ analyzer/img_analyzer.py
@@ -9,8 +9,15 @@ class Analyzer(Base):
         self.TRADE_TABLE = '399300.SZ'
         self.MAP_TABLE = 'map_date'
         self.UPDATE_LIMIT = 2000
+        self.DF_TEMP = None
+        self.GZH_LIST = ['中国证券报', '财新网', '央视财经', '界面新闻']
 
     def map_trade_date(self):
+        """
+        映射交易日期\n
+        :return:
+        """
+
         def extract_trade_date():
             return pd.read_sql(
                 f"SELECT trade_date FROM '{self.TRADE_TABLE}' ", con=self.ENGINE, parse_dates=["trade_date"])
@@ -39,20 +46,25 @@ class Analyzer(Base):
 
         def extract_publish_date():
             df_select = pd.read_sql(
-                f"SELECT id,p_date FROM '{self.ARTICLE_TABLE}' WHERE mov=:mov AND t_date IS NULL LIMIT {self.UPDATE_LIMIT}",
-                con=self.ENGINE, params={'mov': 10}, parse_dates=["p_date"])
+                f"SELECT id,p_date FROM '{self.ARTICLE_TABLE}' WHERE mov=:mov AND p_date BETWEEN :sd AND :ed "
+                f"AND t_date IS NULL LIMIT {self.UPDATE_LIMIT}",
+                con=self.ENGINE,
+                params={'mov': 10,
+                        'sd': int(pd.to_datetime(self.START_DATE).timestamp()),
+                        'ed': int(pd.to_datetime(self.END_DATE).timestamp()), },
+                parse_dates=["p_date"])
             df_select['map_date'] = pd.to_datetime(df_select['p_date'].dt.strftime("%Y-%m-%d 00:00:00"))
             return df_select
 
         def update_by_limit():
             count = 0
             while True:
-                print(count)
                 count += 1
                 df_publish_date = extract_publish_date()
                 if df_publish_date.empty:
                     break
                 else:
+                    print(count)
                     # 匹配
                     df_con = pd.merge(df_publish_date, extract_map_table(), on='map_date', how='left')
                     df_con['t_date'] = (df_con['map_l0']).where(df_con['p_date'] <= df_con['nature_date'],
@@ -68,9 +80,29 @@ class Analyzer(Base):
         update_by_limit()
 
     def cal_img_sentiment(self):
-        pass
+        def extract():
+            df_select = pd.read_sql(
+                f"SELECT a.t_date,gzhs.nickname,a.title,a.cover_local,a.cover_neg "
+                f"FROM gzhs LEFT JOIN {self.ARTICLE_TABLE} AS a ON gzhs.biz = a.biz "
+                "WHERE a.mov=:mov AND a.p_date BETWEEN :sd AND :ed "
+                "AND a.cover_neg IS NOT NULL AND a.t_date IS NOT NULL ",
+                con=self.ENGINE,
+                params={'mov': 10,
+                        'sd': int(pd.to_datetime(self.START_DATE).timestamp()),
+                        'ed': int(pd.to_datetime(self.END_DATE).timestamp())},
+                parse_dates=["t_date"])
+
+            # 转换
+            df_select['t_date'] = df_select['t_date'].dt.strftime("%Y%m%d")
+            return df_select[df_select['nickname'].isin(self.GZH_LIST)]
+
+        def group_by_tdate(df_extract):
+            return df_extract
+
+        self.DF_TEMP = group_by_tdate(extract())
 
 
 if __name__ == '__main__':
     with Analyzer() as Analyzer:
         Analyzer.map_trade_date()
+        Analyzer.cal_img_sentiment()
