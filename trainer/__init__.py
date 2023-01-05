653d09260302b198d300a06a391389cdbb69cf4d
colin <colin_bao@163.com>
2023-01-05 15:21:56 +0800

    update

---
+++ trainer/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2023/1/5 04:56
+# @Author    :Colin
+# @Note      :None
+
+
+if __name__ == "__main__":
+    run_code = 0
