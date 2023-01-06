8084f1078281e35b2e20727810e14d05cb3361bd
colin <colin_bao@163.com>
2023-01-06 21:29:16 +0800

    update

---
+++ notebooks/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :test.py
+# @Time      :2023/1/6 21:05
+# @Author    :Colin
+# @Note      :None
+
+
+if __name__ == "__main__":
+    run_code = 0
---
+++ utils/shs/start_board.sh
@@ -0,0 +1,4 @@
+nohup label-studio start -p 9000 > /home/logs/label_studio.log 2>&1 &
+nohup tensorboard --logdir=/data/Models/TWITTER_SENT_2015/logs --bind_all   > /home/logs/tensorboard.log 2>&1 &
+
+ps -aux | grep tensorboard
